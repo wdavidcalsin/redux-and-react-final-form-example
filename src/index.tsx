d64a2be09f584f42348e2c42a01f6c2aedd0a074
WDavid Calsin <wdavidcalsin968@gmail.con>
2021-03-22 20:09:25 -0500

    feat: 🎸 useDispatch and useSelector with redux

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,9 @@ import { store } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}></Provider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
